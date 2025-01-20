0ac2e6d065d2557b266acbe3494b418bd63dc315
PDLAProject <le.norgeux@gmail.com>
2025-01-20 16:09:53 +0100

    ajout page Info (grad-CAM)

---
+++ app/interface/src/components/InfoAnalysePage.tsx
@@ -0,0 +1,99 @@
+
+import { Alert, Button } from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import DisplayCard from "./DisplayCard";
+import { MagnifyingGlass } from "react-loader-spinner";
+
+
+const InfoAnalysePage: React.FC = () => {
+    const location = useLocation();
+    const API_BASE_URL = 'http://192.168.37.156:50000/predict/image/gradcam';
+    const [loading, setLoading] = useState(false);
+    const { file, fileURL , result, accuracy} = location.state || {};
+    const [grad, setgrad] = useState<File | null>(null);
+    const [gradURL, setgradURL] = useState<string | null>(null);
+    const navigate = useNavigate();
+
+
+    useEffect(() => {
+            const handleDisplayResults = async () => {
+                if (!file) return;
+    
+                setLoading(true);
+                const formData = new FormData();
+                formData.append('image', file);
+    
+                try {
+                    const response = await axios.post(API_BASE_URL, formData, {
+                        headers: {
+                            'Content-Type': 'multipart/form-data',
+                        },
+                    });
+    
+                    const data = response.data;
+                    if (data && typeof data === 'object' && 'grad_cam' in data) {
+                        setgrad(data.grad_cam);
+                        if (grad !== null) {
+                            const imageUrl = URL.createObjectURL(grad);
+                            setgradURL(imageUrl);
+                        }
+                    } else {
+                        console.error("Unexpected response structure:", data);
+                    }
+                } catch (error) {
+                    console.error('Error uploading file:', error);
+                } finally {
+                    setLoading(false);
+                }
+            };
+    
+        handleDisplayResults();
+    }, [file]);
+
+    const handleNavigateToResults = () => {
+        if (file && fileURL && result && accuracy) {
+            const r = result;
+            const a = accuracy;
+            navigate('/results', {
+            state: { file, fileURL, r, a },
+            });
+        }
+        };
+
+    return(
+        <div className="page">
+            <div className="instructions">
+                <p> Grad-CAM of the result </p>
+            </div>
+            {loading ? (
+                <MagnifyingGlass color="#65b2a0" />
+            ) : (
+                <div>
+                    {gradURL && <img src={gradURL} alt="Uploaded" style={{ maxWidth: "100%", maxHeight: "350px" }} />}
+                    <Alert severity="info" sx={{marginBottom: "20px", marginTop: "20px", width: "400px", flexGrow: 1}}>
+                        A Grad-CAM is a technic use in Deep Learning to visualize and understand the decision made by the model.
+                        It generates an heatmap that highlights the crucial regions of an image.
+                        The red areas are the pixels that help the model the most making the decision and the blue ones are the less important.
+                    </Alert>
+                    <DisplayCard result={result} accuracy={accuracy}/>
+                </div>
+            )}
+            <Button 
+            variant="contained"
+            onClick={handleNavigateToResults}
+            sx={{
+              marginTop: "20px",
+              backgroundColor: "#65b2a0",
+              color: "#fff",
+              "&:hover": { backgroundColor: "#386258" },
+            }}>
+                Go back
+            </Button>
+        </div>
+    );
+
+}
+
+export default InfoAnalysePage;
---
+++ app/interface/src/components/ResultsPage.tsx
@@ -0,0 +1,134 @@
+import { Alert, Button } from "@mui/material";
+import { MagnifyingGlass } from "react-loader-spinner";
+import DisplayCard from "./DisplayCard";
+import InfoIcon from '@mui/icons-material/Info';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useLocation, useNavigate } from "react-router-dom";
+
+
+const TypeAlertMessageEnum = {
+    SuccessMessage: 'Given the accuracy, this result is very reliable!',
+    InfoMessage: 'Given the accuracy, this result is fairly reliable',
+    WarningMessage: 'Given the accuracy, this result may be uncertain',
+    ErrorMessage: 'Given the accuracy, this result is uncertain',
+} as const;
+
+const ResultsPage: React.FC = () => {
+    const location = useLocation();
+    const { file, fileURL, r, a } = location.state || {};
+    const [result, setResult] = useState<string | null>(r);
+    const [accuracy, setAccuracy] = useState<number | null>(a);
+    const [loading, setLoading] = useState(false);
+    const [alertMessage, setAlertMessage] = useState("");
+    const API_BASE_URL = 'http://192.168.37.156:50000/predict/image';
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const handleDisplayResults = async () => {
+            if (a === null) {
+                if (!file) return;
+
+                setLoading(true);
+                const formData = new FormData();
+                formData.append('image', file);
+
+                try {
+                    const response = await axios.post(API_BASE_URL, formData, {
+                        headers: {
+                            'Content-Type': 'multipart/form-data',
+                        },
+                    });
+
+                    const data = response.data;
+                    if (data && typeof data === 'object' && 'prediction' in data && 'confidence' in data) {
+                        const confidence = parseFloat(data.confidence.toFixed(2));
+                        setResult(data.prediction);
+                        setAccuracy(confidence);
+
+                        if (confidence <= 60) {
+                            setAlertMessage(TypeAlertMessageEnum.ErrorMessage);
+                        } else if (confidence > 60 && confidence <= 70) {
+                            setAlertMessage(TypeAlertMessageEnum.WarningMessage);
+                        } else if (confidence > 70 && confidence <= 85) {
+                            setAlertMessage(TypeAlertMessageEnum.InfoMessage);
+                        } else {
+                            setAlertMessage(TypeAlertMessageEnum.SuccessMessage);
+                        }
+                    } else {
+                        console.error("Unexpected response structure:", data);
+                    }
+                } catch (error) {
+                    console.error('Error uploading file:', error);
+                } finally {
+                    setLoading(false);
+                }
+            }
+        };
+
+        handleDisplayResults();
+    }, [file]);
+
+    const handleNavigateToInfo = () => {
+        if (file && fileURL && result && accuracy) {
+          navigate('/info', {
+            state: { file, fileURL, result, accuracy },
+          });
+        }
+      };
+
+    return (
+        <div className="page">
+            {fileURL && <img src={fileURL} alt="Uploaded" style={{ maxWidth: "100%", maxHeight: "350px" }} />}
+            <div className="card">
+                {loading ? (
+                    <MagnifyingGlass color="#65b2a0" />
+                ) : (
+                    <div>
+                        <DisplayCard result={result} accuracy={accuracy} />
+                        {alertMessage && (
+                            <Alert severity={
+                                alertMessage === TypeAlertMessageEnum.ErrorMessage
+                                    ? 'error'
+                                    : alertMessage === TypeAlertMessageEnum.WarningMessage
+                                    ? 'warning'
+                                    : alertMessage === TypeAlertMessageEnum.InfoMessage
+                                    ? 'info'
+                                    : 'success'
+                            }>
+                                {alertMessage}
+                            </Alert>
+                        )}
+                    </div>
+                )}
+            </div>
+            <div className="boutons">
+                <Button
+                    variant="contained"
+                    onClick={() => navigate("/home")}
+                    sx={{
+                        backgroundColor: "#65b2a0",
+                        color: "#fff",
+                        "&:hover": { backgroundColor: "#386258" },
+                    }}
+                >
+                    Analyze another image
+                </Button>
+                <Button
+                    variant="contained"
+                    startIcon={<InfoIcon />}
+                    onClick={handleNavigateToInfo}
+                    sx={{
+                        backgroundColor: "#65b2a0",
+                        color: "#fff",
+                        "&:hover": { backgroundColor: "#386258" },
+                    }}
+                >
+                    More info about the analysis
+                </Button>
+            </div>
+        </div>
+    );
+};
+
+export default ResultsPage;
